8b0b66dd79e642741cafd7366ed8549920370376
MRashchynskaya <m.redqueen@gmail.com>
2020-05-20 23:31:18 +0300

    1 Create getter solutions

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,15 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+export function createGetter(field) {
+  return (obj) => {
+    if (!Object.keys(obj).length) return undefined;
+    let storage = obj;
+    let path = field.split(".");
+    for (let value of path) {
+      storage = storage[value];
+    }
 
+    return storage;
+  };
 }
