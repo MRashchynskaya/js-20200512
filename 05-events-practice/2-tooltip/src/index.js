339dc6e701a0002d883f4ce7b9d0061085287bc9
MRashchynskaya <m.redqueen@gmail.com>
2020-06-10 20:58:42 +0300

    Tooltip solution

--- 05-events-practice/2-tooltip/src/index.js
+++ 05-events-practice/2-tooltip/src/index.js
@@ -1,5 +1,66 @@
 class Tooltip {
+  tooltipElement;
+  tooltipTargetElement;
+  offsetRight = 5;
+  offsetBottom = 5;
 
+  constructor() {
+    this.render();
+  }
+
+  addTooltip = (event) => {
+    if (event.target.dataset.tooltip) {
+      this.tooltipTargetElement = event.target;
+      this.tooltipElement.textContent = `${event.target.dataset.tooltip}`;
+      this.tooltipElement.style.display = "block";
+      this.tooltipElement.style.left = event.clientX + this.offsetRight + "px";
+      this.tooltipElement.style.top = event.clientY + this.offsetBottom + "px";
+      this.tooltipTargetElement.addEventListener(
+        "pointerout",
+        this.deleteTooltip
+      );
+      document.addEventListener("pointermove", this.moveTooltip);
+    }
+  };
+
+  moveTooltip = (event) => {
+    this.tooltipElement.style.left = event.clientX + this.offsetRight + "px";
+    this.tooltipElement.style.top = event.clientY + this.offsetBottom + "px";
+  };
+
+  deleteTooltip = (event) => {
+    this.tooltipTargetElement.removeEventListener(
+      "pointerout",
+      this.deleteTooltip
+    );
+    document.removeEventListener("pointermove", this.moveTooltip);
+    this.tooltipElement.style.display = "none";
+  };
+
+  get templateTooltip() {
+    return `<div class="tooltip"></div>`;
+  }
+
+  render() {
+    const node = document.createElement("div");
+    node.innerHTML = this.templateTooltip;
+    this.tooltipElement = node.firstElementChild;
+    this.tooltipElement.style.display = "none";
+    document.body.append(this.tooltipElement);
+  }
+
+  remove() {
+    this.tooltipElement.remove();
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener("pointerover", this.addTooltip);
+  }
+
+  initialize() {
+    document.addEventListener("pointerover", this.addTooltip);
+  }
 }
 
 const tooltip = new Tooltip();
