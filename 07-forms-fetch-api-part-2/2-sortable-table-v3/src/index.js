206a699607fa775b457e9fcef6fe41774a0c2fcd
MRashchynskaya <m.redqueen@gmail.com>
2020-06-12 17:11:47 +0300

    Sortable table 3 solution

--- 07-forms-fetch-api-part-2/2-sortable-table-v3/src/index.js
+++ 07-forms-fetch-api-part-2/2-sortable-table-v3/src/index.js
@@ -1,7 +1,146 @@
-import fetchJson from './utils/fetch-json.js';
+import fetchJson from "./utils/fetch-json.js";
 
-const BACKEND_URL = 'https://course-js.javascript.ru';
+const BACKEND_URL = "https://course-js.javascript.ru";
 
-export default class SortableTable {
+class SortableTable {
+  element;
+  subElements = {};
+  header = [];
+  data = [];
+  url = "";
+  defaultSort = {};
+  query = "";
 
+  constructor(header, { url } = {}) {
+    this.header = header;
+    this.url = `${BACKEND_URL}/${url}`;
+    this.defaultSort = {
+      _sort: header.find((item) => item.sortable).id,
+      _order: "asc",
+      _start: 0,
+      _end: 30,
+    };
+    this.query = `?${new URLSearchParams(this.defaultSort).toString()}`;
+    this.getData(this.url, this.query);
+    this.render();
+  }
+
+  get templateTableContainer() {
+    return `
+     <div data-element="productsContainer" class="products-list__container">
+       <div class="sortable-table">
+        ${this.templateTableHeader}
+        <div data-element="body" class="sortable-table__body">
+        ${this.templateTableContent}
+        </div>
+       </div>
+     </div>
+    `;
+  }
+
+  get templateTableHeader() {
+    return `
+      <div data-element="header" class="sortable-table__header sortable-table__row">
+        ${this.header
+          .map(({ id, title, sortable }) => {
+            const order =
+              this.defaultSort._sort === id ? this.defaultSort._order : "desc";
+            return `
+            <div class="sortable-table__cell" data-name='${id}' data-sortable='${sortable}' data-order='${order}'>
+              <span>${title}</span>
+              ${this.getHeaderSortArrow(id)}
+            </div>
+          `;
+          })
+          .join("")}
+      </div>
+    `;
+  }
+
+  getHeaderSortArrow(id) {
+    const isOrderExist =
+      this.defaultSort._sort === id ? this.defaultSort._order : "";
+    return isOrderExist
+      ? `<span data-element="arrow" class="sortable-table__sort-arrow">
+          <span class="sort-arrow"></span>
+        </span>`
+      : "";
+  }
+
+  get templateTableContent() {
+    return this.data
+      .map((dataItem) => {
+        let tableRow = this.header
+          .map((headerItem) => {
+            return headerItem.template
+              ? headerItem.template(dataItem[headerItem.id])
+              : `<div class="sortable-table__cell">${
+                  dataItem[headerItem.id]
+                }</div>`;
+          })
+          .join("");
+        return `<a href="/products/${dataItem.id}" class="sortable-table__row">
+          ${tableRow}
+          </a>
+        `;
+      })
+      .join("");
+  }
+
+  async getData(url, query) {
+    let response = await fetchJson(`${url}${query}`);
+    this.data = response;
+    this.subElements.body.innerHTML = this.templateTableContent;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.templateTableContainer;
+    const element = wrapper.firstElementChild;
+    this.element = element;
+    this.subElements = this.getSubElements(element);
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    this.subElements.header.addEventListener("pointerdown", this.sortOnServer);
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll("[data-element]");
+    const subElements = {};
+    for (let subElement of elements) {
+      subElements[subElement.dataset.element] = subElement;
+    }
+    return subElements;
+  }
+
+  sortOnServer = (event) => {
+    const targetCell = event.target.closest('[data-sortable="true"]');
+    const currentSortParameter =
+      targetCell.dataset.order === "asc" ? "desc" : "asc";
+    const currentSort = this.defaultSort;
+    currentSort._sort = targetCell.dataset.name;
+    currentSort._order = currentSortParameter;
+    const currentQuery = `?${new URLSearchParams(currentSort).toString()}`;
+    this.element.classList.add("sortable-table_loading");
+    this.subElements.body.innerHTML = "";
+    this.getData(this.url, currentQuery);
+    targetCell.dataset.order = currentSortParameter;
+    this.element.classList.remove("sortable-table_loading");
+    const arrow = targetCell.querySelector(".sortable-table__sort-arrow");
+    if (!arrow) {
+      targetCell.append(this.subElements.arrow);
+    }
+  };
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
+
+export default SortableTable;
